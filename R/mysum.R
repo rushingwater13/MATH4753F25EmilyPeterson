b5a5bfb634355f76ab3e79676ad0f6e9a5daa407
rushingwater13 <rushingwater13@gmail.com>
2025-09-15 12:22:26 -0500

    Title change

---
+++ R/mysum.R
@@ -0,0 +1,12 @@
+#' A new summary function
+#'
+#' @param x A numeric vector.
+#'
+#' @returns A summary of the properties of 'x'.
+#' @export
+#'
+#' @examples
+#' summary(1:10)
+mysum <- function(x) {
+  summary(x)
+}
